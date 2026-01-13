30e504303e074ed93d71649a98a48fa0bac05eb2
budipermana16 <gilangbudi50@gmail.com>
2026-01-13 18:39:31 +0700

    Modul 2

---
+++ app/src/main/java/com/example/modul2/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.modul2;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+//        setContentView(com.example.modul2.R.layout.activity_main)
+//        setContentView(R.layout.relative_layout)
+
+//        val gridView = findViewById<View>(R.id.grid_view) as GridView
+
+//        gridView.adapter = ImageAdapter(this)
+    }
+}
